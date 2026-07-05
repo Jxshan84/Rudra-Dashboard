6d2a1ad147077b7811066e7cf0e509fc5da580e6
Jxshan84 <sasukeuchicha46535@gmail.com>
2026-07-06 00:19:24 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,26 @@
-placeholder 
+const passport = require("passport");
+const DiscordStrategy = require("passport-discord").Strategy;
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+passport.use(
+  new DiscordStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.REDIRECT_URI,
+      scope: ["identify", "guilds"]
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
+
+module.exports = passport;
